6d4eb190139b62ca1a6e1c3b6e51b2d7b4d7ddac
sipviktor <96287934+sipviktor@users.noreply.github.com>
2022-08-21 22:14:08 +0100

    Add files via upload

---
+++ _hero.php
@@ -0,0 +1,8 @@
+<div class="modro">
+    <div class="hero-image">
+        <div class="hero-text">
+            <h1>Naskočte s námi na tu správnou vlnu!</h1>
+            <p>Tu správnou vlnu s námi můžete chytit v Egyptě. Naše zážitkové zájezdy se specializují na pobyt na safari lodi na Rudém moři s možností plavání s volně žijícími delfíny nebo s možností potápěčských výprav.  A kdo má radši pevninu, tak se s námi může vrátit o několik tisíc let zpět při návštěvách starověkých egyptských chrámů a památek v údolí Nilu s pobytem na luxusní hotelové lodi.</p>
+        </div>
+    </div>
+</div>
